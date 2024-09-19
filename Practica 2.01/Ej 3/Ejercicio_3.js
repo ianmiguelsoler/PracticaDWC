f8ecdb8a78e7dbf2f31f8c74b60d5e855c645089
ianmiguelsoler <ianmiguel@gmail.com>
2024-09-19 13:51:40 +0200

    ejercicio 4 terminado

--- Practica 2.01/Ej 3/Ejercicio_3.js
+++ Practica 2.01/Ej 3/Ejercicio_3.js
@@ -5,8 +5,11 @@ var numero1 = 2 /*numero impresion*/
 var numero2 = 8 /*numero multiplicador*/
 
 //? Soluciones
-console.log(multiplicar(numero1, numero2))
-
+if (numero1 >= 0) {
+    console.log(multiplicar(numero1, numero2)); // Solo se ejecuta si número 1 es >= 0
+} else {
+    console.log("Los números no cumplen con las condiciones: numero1 debe ser >= 0.");
+}
 //* Funciones
 // Función que muestra un numero x de veces el siguiente multiplicado por 2
 function multiplicar(numero1P, numero2M) {
