bae11f249e9f158af56e64f6dddffe704e5e6503
ianmiguelsoler <ianmiguel@gmail.com>
2024-09-19 16:48:00 +0200

    terminado

--- Practica 2.01/Ej5/Ejercicio_5.js
+++ Practica 2.01/Ej5/Ejercicio_5.js
@@ -1,28 +1,32 @@
 "use strict";
 
 //! Variables
-var alturaMarcos = 170 /*cm*/
-var pesoMarcos = 80 /*Kg*/
-
-var alturaJuan = 150 /*cm*/
-var pesoJuan = 57 /*Kg*/
+var totalGastado = [124,48,268] /*Dolares gastados*/
+var propinas = [20,15,10] /*Dolares gastados*/
+// var totalGastadoP = [124,48,268] /*Dolares gastados*/
 
 //? Solución
-console.log("¿Tiene Marcos un IMC mayor que el de Juan?: "+ calculoPeso(alturaMarcos, pesoMarcos, alturaJuan, pesoJuan))
+console.log(calculadoraDePropinas(totalGastado, propinas))
 
 
 //* Funciones
-function calculoPeso(alturaMarcosF, pesoMarcosF, alturaJuanF, pesoJuanF) {
+//Esta función calcula las propinas según unos valores pasados en un array
+function calculadoraDePropinas(totalGastado, propinasPorcentajes) {
+    let totalPropinas = [];
+    let totalConPropinas = [];
     
-    let imcMarcos = pesoMarcosF / Math.pow(alturaMarcosF, 2); // Convertimos cm a metros
-    let imcJuan = pesoJuanF / Math.pow(alturaJuanF, 2); // Convertimos cm a metros
-
-    let marcosEsMayorF = imcMarcos > imcJuan; // Verificamos si el IMC de Marcos es mayor
-
-    if(marcosEsMayorF){
-        return true
+    for (let contador = 0; contador < totalGastado.length; contador++) {
+        if (totalGastado[contador] < 50) {
+            totalPropinas[contador] = totalGastado[contador] * (propinasPorcentajes[0] / 100);
+        } else if (totalGastado[contador] >= 50 && totalGastado[contador] <= 200) {
+            totalPropinas[contador] = totalGastado[contador] * (propinasPorcentajes[1] / 100);
+        } else if (totalGastado[contador] > 200) {
+            totalPropinas[contador] = totalGastado[contador] * (propinasPorcentajes[2] / 100);
+        }
     }
-    else{
-        return false
+
+    for (let contador = 0; contador < totalGastado.length; contador++) {
+        totalConPropinas[contador] = totalGastado[contador] + totalPropinas[contador];
+        console.log(`Ha pagado en el restaurante Nº ${contador+1}, una factura de ${totalGastado[contador]}$, una propina de ${totalPropinas[contador].toFixed(2)}$, y el total fue ${totalConPropinas[contador].toFixed(2)}$.`);
     }
 }
