b0bea4cb6821dbc828febcb34c20ceb2d75d0978
ianmiguelsoler <ianmiguel@gmail.com>
2024-09-20 19:52:09 +0200

    ejercicio 1 y 2 hecho

--- Practica 2.03/Ej 2/Ejercicio_2.js
+++ Practica 2.03/Ej 2/Ejercicio_2.js
@@ -1,8 +1,37 @@
 "use strict";
 
 //! Variables
+var nombreCurso = "2ºDAW"
+var anoCurso = "2024"
+var descCurso = "Es un curso donde se aprende programación Web"
+var alumnado = []
 
 //? Solución
+var curso = creaCurso(nombreCurso, anoCurso, descCurso, alumnado);
+muestraCurso(curso)
 
 //* Funciones
+function creaCurso(nombreCurso, anoCurso, descCurso, alumnado){
+    return {
+        nombreCurso: nombreCurso,
+        anoCurso: anoCurso,
+        descCurso: descCurso,
+        alumnado: alumnado
+    };
+};
+
+function muestraCurso(curso){
+    console.log(`El Curso Contiene esto:\n`)
+    for (const clave in curso) {
+        if (Object.hasOwnProperty.call(curso, clave)) {
+            const element = curso[clave];
+            if (element !== null && element !== undefined && element.length !== 0) {
+                console.log(`${clave} => ${element}`);
+            }
+            else{
+                console.log(`${clave} => NO TIENE NINGÚN DATO`);
+            }
+        }
+    }
+};
 
