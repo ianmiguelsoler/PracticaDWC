1de7b63e5994249226f02fd6144dbaafdf4014c0
ianmiguelsoler <ianmiguel@gmail.com>
2024-09-23 20:23:31 +0200

    arreglado ejercicio 1

--- Practica 2.04/Ej 1/Ejercicio_1.js
+++ Practica 2.04/Ej 1/Ejercicio_1.js
@@ -1,27 +1,33 @@
 "use strict";
 
 //! Variables
-var arrayCadenas = ["Ian","Pedro","Miguel","Francisco","Julio"];
+const arrayCadenas = ["Ian", "Pedro", "Miguel", "Francisco", "Julio"];
 
 //? Solución
-//Array que pone mayúsculas todo el array.
-var arrayCadenasMayus = arrayCadenas.map(mostrarEnMayuscula);
-console.log(arrayCadenasMayus); 
-//Array ordenado con sort, no hace falta mapearlo.
-var arrayOrdenado = arrayCadenasMayus.sort();
-console.log(arrayOrdenado); 
+// Array que pone en mayúsculas todo el array usando spread operator y map.
+const arrayCadenasMayus = [...arrayCadenas].map(mostrarEnMayuscula);
+console.log(arrayCadenasMayus);
 
-//array mapeado con JSON.
-var arrayJSON = arrayCadenas.map((nombre, indice) => {
+// Array ordenado con sort, usando spread operator para no modificar el original.
+const arrayOrdenado = [...arrayCadenasMayus].sort();
+console.log(arrayOrdenado);
+
+// Array mapeado a JSON con spread operator y desestructuración.
+const arrayJSON = arrayCadenas.map((nombre, indice) => {
     return {
         id: indice,
-        nombre: nombre
+        nombre
     };
 });
-console.log(arrayJSON)
-//* Funciones
-//Función que se le pasa el array y lo devulve en mayúsculas.
-function mostrarEnMayuscula(valor, indice, arrayCadenas){
-    return valor.toUpperCase();
-};
+console.log(arrayJSON);
 
+//* Funciones
+// Función que recibe el array y lo devuelve en mayúsculas.
+function mostrarEnMayuscula(valor) {
+    return valor.toUpperCase();
+}
+// Usamos map en lugar de forEach para recorrer el array y devolver un nuevo array
+const nombresConId = arrayJSON.map(({ id, nombre }) => {
+    return `ID: ${id}, Nombre: ${nombre}`;
+});
+console.log(nombresConId);
