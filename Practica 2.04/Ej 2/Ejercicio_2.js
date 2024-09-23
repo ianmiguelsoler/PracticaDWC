4b3497feaae74979110a92da439afcd8e6975591
ianmiguelsoler <ianmiguel@gmail.com>
2024-09-23 20:37:40 +0200

    ej 2 terminado

--- Practica 2.04/Ej 2/Ejercicio_2.js
+++ Practica 2.04/Ej 2/Ejercicio_2.js
@@ -1,38 +1,52 @@
 "use strict";
 
 //! Variables
-var nombreCurso = "2ºDAW"
-var anoCurso = "2024"
-var descCurso = "Es un curso donde se aprende programación Web"
-var alumnado = []
+let array1 = numerosAleatorios(10);
+let array2 = numerosAleatorios(6);
+let array3 = numerosAleatorios(14);
 
 //? Solución
-var curso = creaCurso(nombreCurso, anoCurso, descCurso, alumnado);
-muestraCurso(curso)
 
 //* Funciones
-//Función que es capaz de crear un curso.
-function creaCurso(nombreCurso, anoCurso, descCurso, alumnado){
-    return {
-        nombreCurso: nombreCurso,
-        anoCurso: anoCurso,
-        descCurso: descCurso,
-        alumnado: alumnado
-    };
-};
-//Muestra el curso y también los que sean indefinidos, nulos o no tengan tamaño si son array. Mostrará un mensaje en él.
-function muestraCurso(curso){
-    console.log(`El Curso Contiene esto:\n`)
-    for (const clave in curso) {
-        if (Object.hasOwnProperty.call(curso, clave)) {
-            const element = curso[clave];
-            if (element !== null && element !== undefined && element.length !== 0) {
-                console.log(`${clave} => ${element}`);
-            }
-            else{
-                console.log(`${clave} => NO TIENE NINGÚN DATO`);
-            }
-        }
-    }
-};
 
+//Genera array aleatorios pasándole la longitud de estos
+function numerosAleatorios(numero) {
+    let arrayAleatorio = [];
+    for (let contador = 0; contador < numero; contador++) {
+        arrayAleatorio[contador] = Math.floor(Math.random() * 100) + 1; 
+    }
+    return arrayAleatorio;
+}
+
+//Imprime el array de forma formateada
+function imprimirArrayFormateado(array) {
+    console.log("[" + array.join(", ") + "]");
+}
+
+// Función que une los arrays y se queda solo con los números mayores a 5
+function creaArrayMayor(...arrays) {
+    // Unir todos los arrays en uno
+    let arrayUnido = [].concat(...arrays);
+    let contador = 0
+    console.log('El array antes de filtrarse es este:')
+    imprimirArrayFormateado(arrayUnido)
+    // Filtrar solo los números mayores a 5
+    let arrayFiltrado = arrayUnido.filter(num => {
+        if (num > 5) {
+            contador++; 
+            return true; 
+        }
+        return false;
+    });
+    console.log(`Se han eliminado un total de ${contador}, números.`)
+    return arrayFiltrado;
+}
+console.log(`Los arrays son estos:`)
+imprimirArrayFormateado(array1)
+imprimirArrayFormateado(array2)
+imprimirArrayFormateado(array3)
+// Llamada a la función con múltiples arrays
+let arrayMayor = creaArrayMayor(array1, array2, array3);
+
+//El array después de filtrarse es este
+imprimirArrayFormateado(arrayMayor);
