c5872408a61c4c481fbc65722d92fadc50062870
ianmiguelsoler <ianmiguel@gmail.com>
2024-09-27 17:42:03 +0200

    ej 1 terminado

--- Practica 2.05/Libreria_js/Ejercicio_1.js
+++ Practica 2.05/Libreria_js/Ejercicio_1.js
@@ -1,33 +1,45 @@
 "use strict";
 
 //! Variables
-var nombreCurso = "2ºDAW"
-var anoCurso = "2024"
-var descCurso = "Es un curso donde se aprende programación Web"
-var alumnado = []
+
 //? Solución
-//* En esta cadena a la hora de pedirlo, me muestra el objeto porque solo puede impirmir cadenas de texto, tendria que 
-//*utilizar JSON.stringify para que me imprimera una cadena de text
-// let curso = creaCurso(nombreCurso, anoCurso, descCurso, alumnado)
-// console.log(`El objeto creado es ${curso}`)
-const curso2 = creaCurso(nombreCurso, anoCurso, descCurso, alumnado);
-console.log("El objeto creado es:", curso);
+const cani = cadenaCani("Pedro es un cani y lo sabes");
+console.log("Su cadena de texto se ha vuelto cani, te lo muestro a continuación:\n", cani);
 
 //* Funciones
-//Función que se le pasan los datos y te crea un Curso.
-function creaCurso(nombreCurso, anoCurso, descCurso, alumnado){
-    return {
-        nombreCurso: nombreCurso,
-        anoCurso: anoCurso,
-        descCurso: descCurso,
-        alumnado: alumnado
-    };
+function cadenaCani(cadenaCani) {
+    var resultado = "";
+    //Hacemos un array recorriendo todo el string caracter a caracter
+    for (let contador = 0; contador < cadenaCani.length; contador++) {
+        let caracter = cadenaCani[contador];
 
+        // Si el carácter está en mayúsculas, lo cambiamos a minúsculas
+        if (caracter === caracter.toUpperCase()) {
+            if(caracter === 'c' || caracter === 'C'){
+                resultado += 'k'
+            }
+            else{
+                resultado += caracter.toLowerCase();
+            }
+        } 
+        // Si está en minúsculas, lo cambiamos a mayúsculas
+        else {
+            if(caracter === 'c' || caracter === 'C'){
+                resultado += 'k'
+            }
+            else{
+                resultado += caracter.toUpperCase();
+            }
+        }
+    }
+    let cuantasH = (Math.random() * 4) + 1;
+        for (let contador = 0; contador < cuantasH; contador++) {
+            resultado += 'H';  
+        }
+    return resultado; // Devolvemos el string modificado
 };
 
-
-// Exporta los elementos situados entre las llaves.
-export { sumar, restar, multiplicar };
-// Exporta el elemento con un seudónimo.
-export { multiplicar as multi };
-
+// // Exporta los elementos situados entre las llaves.
+// export { sumar, restar, multiplicar };
+// // Exporta el elemento con un seudónimo.
+// export { multiplicar as multi };
